96e0eee41b5b2059080f0469a046d2619899128a
Daniel Martling <daniel.martling@proton.me>
2026-06-16 23:31:02 +0200

    static pages

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,19 @@
 "use strict"
 
 const express = require('express');
+const path    = require('path');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.get('/', function (req, res) {
+    res.sendFile('../client/index.html');
 });
 
 app.listen(port, () => {
